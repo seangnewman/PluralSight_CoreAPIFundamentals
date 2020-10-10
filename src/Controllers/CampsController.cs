d6afca216dcd4328547901b45a629f55f8986dff
newmanuevers <seangnewman@msn.com>
2020-10-10 11:25:29 -0700

    Walkthrough of Core 2.2 API

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CampsController(ICampRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<CampModel[]>> Get( bool includeTalks = false)
+        {
+
+             
+
+            try
+            {
+                var results = await _repository.GetAllCampsAsync(includeTalks);
+                 return _mapper.Map<CampModel[]>(results);
+ 
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Error");
+            }
+           
+        }
+
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampModel> > Get(string moniker)
+        {
+            try
+            {
+                var result = await  _repository.GetCampAsync(moniker);
+                if (result == null)
+                    return NotFound();
+                else
+                    return _mapper.Map<CampModel>(result);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Error"); 
+            }
+        }
+        [HttpGet("search")]
+        public async Task<ActionResult<CampModel[]>> SearchByDate(DateTime theDate, bool includeTalks = false)
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsByEventDate(theDate, includeTalks);
+                if (!results.Any())
+                    return NotFound();
+                return _mapper.Map<CampModel[]>(results);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Error");
+            }
+        }
+
+
+
+    }
+}
