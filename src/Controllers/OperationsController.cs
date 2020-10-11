c7edb31c9e246834023301b40e59a0e4bdd3fe3b
newmanuevers <seangnewman@msn.com>
2020-10-11 16:09:24 -0700

    Completed functional API example using OPTIONS as verb

---
+++ src/Controllers/OperationsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreCodeCamp.Controllers
+{
+   [Route("api/[controller]")]
+    public class OperationsController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public OperationsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot) _config;
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+
+               return this. StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+    }
+}
